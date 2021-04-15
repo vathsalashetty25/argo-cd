c1eabc3235a27684024b247ef4ef53c197704828
vathsalashetty96 <82566301+vathsalashetty96@users.noreply.github.com>
2021-04-15 14:08:02 +0530

    Update update-openapi.sh

--- hack/update-openapi.sh
+++ hack/update-openapi.sh
@@ -17,8 +17,8 @@ export GO111MODULE=on
 [ -e ./v2 ] || ln -s . v2
 ./dist/openapi-gen \
   --go-header-file ${PROJECT_ROOT}/hack/custom-boilerplate.go.txt \
-  --input-dirs github.com/argoproj/argo-cd/v2/pkg/apis/application/${VERSION} \
-  --output-package github.com/argoproj/argo-cd/v2/pkg/apis/application/${VERSION} \
+  --input-dirs github.com/vathsalashetty96/argo-cd/v2/pkg/apis/application/${VERSION} \
+  --output-package github.com/vathsalashetty96/argo-cd/v2/pkg/apis/application/${VERSION} \
   --report-filename pkg/apis/api-rules/violation_exceptions.list \
   $@
 [ -e ./v2 ] && rm -rf v2
