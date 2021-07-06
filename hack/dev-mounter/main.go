718c3414ef6a48dcf92df1e1e37ce7441958194f
vathsalashetty25 <70144077+vathsalashetty25@users.noreply.github.com>
2021-07-06 19:09:16 +0530

    Update main.go

--- hack/dev-mounter/main.go
+++ hack/dev-mounter/main.go
@@ -19,7 +19,7 @@ import (
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 
-	"github.com/vathsalashetty25/argo-cd/util/cli"
+	"github.com/argoproj/argo-cd/util"
 
 	// load the gcp plugin (required to authenticate against GKE clusters).
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
