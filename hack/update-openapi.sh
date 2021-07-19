a517dc83bfdf27fa78136501df7e8a9437837c01
vathsalashetty25 <70144077+vathsalashetty25@users.noreply.github.com>
2021-07-19 14:35:41 +0530

    Update update-openapi.sh

--- hack/update-openapi.sh
+++ hack/update-openapi.sh
@@ -4,20 +4,7 @@ set -o nounset
 #set -o pipefail
 
 PROJECT_ROOT=$(cd $(dirname "$0")/.. ; pwd)
-CODEGEN_PKG=${PROJECT_ROOT}/vendor/k8s.io/kube-openapi
-VERSION="v1alpha1"
 
-export GO111MODULE=off
-go build -o dist/openapi-gen ${CODEGEN_PKG}/cmd/openapi-gen
-
-
-
-./dist/openapi-gen \
-  --go-header-file ${PROJECT_ROOT}/hack/custom-boilerplate.go.txt \
-  --input-dirs github.com/vathsalashetty25/argo-cd/pkg/apis/application/${VERSION} \
-  --output-package github.com/vathsalashetty25/argo-cd/pkg/apis/application/${VERSION} \
-  --report-filename pkg/apis/api-rules/violation_exceptions.list \
-  $@
 
 go build -o ./dist/gen-crd-spec ${PROJECT_ROOT}/hack/gen-crd-spec
 ./dist/gen-crd-spec
