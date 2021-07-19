fd823256354c094922e3d6f09227562a92f7800f
vathsalashetty25 <70144077+vathsalashetty25@users.noreply.github.com>
2021-07-19 12:06:00 +0530

    Update update-openapi.sh

--- hack/update-openapi.sh
+++ hack/update-openapi.sh
@@ -3,7 +3,7 @@ set -o errexit
 set -o nounset
 #set -o pipefail
 
-PROJECT_ROOT=$(cd $(dirname "$0")/.. ; pwd)
+PROJECT_ROOT=/go/src/github.com/argoproj/argo-cd
 CODEGEN_PKG=${PROJECT_ROOT}/vendor/k8s.io/kube-openapi
 VERSION="v1alpha1"
 
@@ -14,10 +14,10 @@ go build -o dist/openapi-gen ${CODEGEN_PKG}/cmd/openapi-gen
 
 ./dist/openapi-gen \
   --go-header-file ${PROJECT_ROOT}/hack/custom-boilerplate.go.txt \
-  --input-dirs github.com/vathsalashetty25/argo-cd/pkg/apis/application/${VERSION} \
-  --output-package github.com/vathsalashetty25/argo-cd/pkg/apis/application/${VERSION} \
+  --input-dirs github.com/argoproj/argo-cd/pkg/apis/application/${VERSION} \
+  --output-package github.com/argoproj/argo-cd/pkg/apis/application/${VERSION} \
   --report-filename pkg/apis/api-rules/violation_exceptions.list \
   $@
 
-go build -o ./dist/gen-crd-spec github.com/argoproj/argo-cd/hack/gen-crd-spec
+go build -o ./dist/gen-crd-spec ${PROJECT_ROOT}/hack/gen-crd-spec
 ./dist/gen-crd-spec
